4218642e1ff299b6e75e1eb5b5f3d615554b8ede
karazhyn <h00dinixx12@gmail.com>
2021-05-25 23:05:06 +0300

    bugfix

--- links_crawler/main_crawler.py
+++ links_crawler/main_crawler.py
@@ -31,6 +31,6 @@ def main_crawler(initial_link, initial_thread=2, initial_depth=1):
 
     return crawler.all_links, crawler.external_links
 
-#debug block
+#crawler debug block
 # if __name__ == '__main__':
 #     main_crawler('https://support.unity3d.com/hc/en-us', 20)
--- main.py
+++ main.py
@@ -9,54 +9,58 @@ import links_crawler.main_crawler
 # import logging
 # from LinksCrawler.config import *
 # from LinksCrawler.crawler import Crawler
-# try:
-st.set_page_config(
-    page_title="GraphMe",
-    page_icon=None,
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-# except: 
-#     pass
-
-# Title
-st.title("GraphMe")
-st.markdown("## Visualize all connections between site references")
-# Sidebar
-st.sidebar.title('Select graph options')
-link_input = st.sidebar.text_input("Enter link of the site to vizualize:", "https://support.unity3d.com/hc/en-us", key='input1')
-depth_input =  st.sidebar.text_input("Enter depth of crawling:", "2", key='input2')
-thread_input =  st.sidebar.text_input("Enter number of threads to crawling:", "20", key='input3')
-option=st.sidebar.selectbox('Select type',('Full links','Domains only'), key='box')
-# style=st.sidebar.checkbox('Add style interactivity') #feature does not work right now
 
 
-# display the name when the submit button is clicked
-# .title() is used to get the input text string 
-if(st.sidebar.button('Graph it!', key='button1')):
-    if re.match('^http.*', link_input.title().lower()):
-        initial_link = link_input.title().lower()
-    else:
-        initial_link = 'http://'+link_input.title().lower()
+#try\except block made for handling st.set_page_config() bug
+def main():
+    st.set_page_config(
+        page_title="GraphMe",
+        page_icon=None,
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
 
-    all_links, external_links = links_crawler.main_crawler.main_crawler(initial_link, int(thread_input.title()), int(depth_input.title())) #returns 2 dicts
-    
-    print(external_links)
-    done = graph.make_graph(initial_link, all_links, external_links, option)
-    
-    if done:
-        HtmlFile = open("graphs/graphme.html", 'r', encoding='utf-8')
+
+    # Title
+    st.title("GraphMe")
+    st.markdown("## Visualize all connections between site references")
+    # Sidebar
+    st.sidebar.title('Select graph options')
+    link_input = st.sidebar.text_input("Enter link of the site to vizualize:", "https://support.unity3d.com/hc/en-us", key='input1')
+    depth_input =  st.sidebar.text_input("Enter depth of crawling:", "2", key='input2')
+    thread_input =  st.sidebar.text_input("Enter number of threads to crawling:", "20", key='input3')
+    option=st.sidebar.selectbox('Select type',('Full links','Domains only'), key='box')
+    # style=st.sidebar.checkbox('Add style interactivity') #feature does not work right now
+
+
+    # display the name when the submit button is clicked
+    # .title() is used to get the input text string 
+    if(st.sidebar.button('Graph it!', key='button1')):
+        if re.match('^http.*', link_input.title().lower()):
+            initial_link = link_input.title().lower()
+        else:
+            initial_link = 'http://'+link_input.title().lower()
+
+        all_links, external_links = links_crawler.main_crawler.main_crawler(initial_link, int(thread_input.title()), int(depth_input.title())) #returns 2 dicts
+        
+        print(external_links)
+        done = graph.make_graph(initial_link, all_links, external_links, option)
+        
+        if done: #if make_graph() was sucessfully done
+            HtmlFile = open("graphs/graphme.html", 'r', encoding='utf-8')
+            source_code = HtmlFile.read() 
+            components.html(source_code, height = 800,width=1300)
+
+    st.sidebar.title('Select saved graph')
+
+    files = os.listdir('graphs/')
+    files = [x for x in files if re.search("^.*html$", x)]
+    saved_graphs = st.sidebar.selectbox('Show saved graphs',files)
+
+    if(st.sidebar.button('Show it!', key='button2')):
+        HtmlFile = open('graphs/'+saved_graphs.title().lower(), 'r', encoding='utf-8')
         source_code = HtmlFile.read() 
         components.html(source_code, height = 800,width=1300)
 
-st.sidebar.title('Select saved graph')
-
-files = os.listdir('graphs/')
-files = [x for x in files if re.search("^.*html$", x)]
-saved_graphs = st.sidebar.selectbox('Show saved graphs',files)
-
-if(st.sidebar.button('Show it!', key='button2')):
-
-    HtmlFile = open('graphs/'+saved_graphs.title().lower(), 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    components.html(source_code, height = 800,width=1300)
+if __name__ == '__main__':
+    main()
